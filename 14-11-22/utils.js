45d190f2d2971f8b6cdbcddd2878c99222b50d4b
ElisaAzzolina <creative.azzolina@gmail.com>
2022-11-14 16:31:17 +0100

    ADD logic

---
+++ 14-11-22/utils.js
@@ -0,0 +1,22 @@
+/**
+ * Take an element that matches with che element of the DOM passed as argument
+ * 
+ * @param {string} el - specific DOM element
+ * @returna HTMLelement
+ */
+ const qs = (el) => document.querySelector(el);
+
+
+ /**
+  * Send a GET request to the end-point specified as argument
+  * 
+  * @param {string} url
+  * @returns data
+  */
+ const GET = async url => {
+     const res = await fetch(url);
+     const data = await res.json();
+     return await data;
+ }
+ 
+ export {qs, GET};
