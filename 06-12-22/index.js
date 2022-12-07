a41a8d9cfbb1f4a6098138dfbb0a1fce1b6dae61
ElisaAzzolina <creative.azzolina@gmail.com>
2022-12-07 10:33:26 +0100

    esercitazione 06/07-12-22

---
+++ 06-12-22/index.js
@@ -0,0 +1,143 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const app = express();
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+//pagina home
+app.get("/home", function (req, res) {
+  res.sendFile("index.html", { root: __dirname + "/src/html" });
+});
+
+// SERVER - MONGOOSE
+mongoose.connect("mongodb://127.0.0.1:27017/videoteca").then(
+  app.listen(3000, () => {
+    console.log("server is listening");
+  })
+);
+
+// MODEL ATTORI
+const actSchema = new mongoose.Schema({
+  nome: String,
+  cognome: String,
+  data_nascita: String,
+  riconoscimenti: String,
+  inizio_attivita: String,
+  fine_attivita: String,
+  in_attivita: Boolean,
+});
+
+const Actor = mongoose.model("Actor", actSchema);
+
+// MODEL REGISTI
+const dirSchema = new mongoose.Schema({
+  nome: String,
+  cognome: String,
+  data_nascita: String,
+  riconoscimenti: String,
+  inizio_attivita: String,
+  fine_attivita: String,
+  in_attivita: Boolean,
+});
+const Director = mongoose.model("Director", dirSchema);
+
+// GET ATTORI - MONGODB
+const getAllActors = async (req, res) => {
+  try {
+    const actors = await Actor.find();
+    res.status(200).json(actors);
+  } catch (err) {
+    res.status(404).json({ err: "ERR" });
+  }
+};
+app.get("/attori", getAllActors);
+
+// POST ATTORE - MONGODB
+const postActor = async (req, res) => {
+  const actor = req.body;
+  const newActor = new Actor(actor);
+
+  try {
+    await newActor.save();
+    res.status(201).json(newActor);
+  } catch (err) {
+    res.status(409).json({ err: "ERR" });
+  }
+};
+app.post("/attore", postActor);
+
+//DELETE ATTORE - MONGODB
+const deleteActor = async (req, res) => {
+  const id = req.body.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ err: "id non conforme" });
+  }
+  try {
+    await Actor.findByIdAndDelete(id);
+    res.status(200).json({ msg: "Attore eliminato." });
+  } catch (err) {
+    res.status(409).json({ err: "ERR" });
+  }
+};
+app.delete("/attore", deleteActor);
+
+//PUT ATTORE - MONGODB
+/* const putActor = async (req, res) => {
+  const id = req.query.id;
+  const body = { ...req.body };
+  console.log(id);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ err: "id non conforme" });
+  }
+  try {
+    await Actor.findByIdAndUpdate(id, body, { new: true });
+    res.status(200).json({ msg: "Attore aggiornato!" });
+  } catch (err) {
+    res.status(404).json({ err: "ERR" });
+  }
+};
+app.put("/attore", putActor); */
+
+/*----- REGISTI -----*/
+
+// GET REGISTI - MONGODB
+const getAllDirectors = async (req, res) => {
+  try {
+    const directors = await Director.find();
+    res.status(200).json(directors);
+  } catch (err) {
+    res.status(404).json({ err: "ERR" });
+  }
+};
+app.get("/registi", getAllDirectors);
+
+// POST REGISTA - MONGODB
+const postDirector = async (req, res) => {
+  const director = req.body;
+  const newDirector = new Director(director);
+
+  try {
+    await newDirector.save();
+    res.status(201).json(newDirector);
+  } catch (err) {
+    res.status(409).json({ err: "ERR" });
+  }
+};
+app.post("/regista", postDirector);
+
+//DELETE REGISTA - MONGODB
+const deleteDirector = async (req, res) => {
+  const id = req.body.id;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    res.status(404).json({ err: "id non conforme" });
+  }
+  try {
+    await Director.findByIdAndDelete(id);
+    res.status(200).json({ msg: "Regista eliminato." });
+  } catch (err) {
+    res.status(409).json({ err: "ERR" });
+  }
+};
+app.delete("/regista", deleteDirector);
